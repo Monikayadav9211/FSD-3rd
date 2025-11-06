f83b26a1c5143c994c39963752cc384c07009476
Monikayadav9211 <monikayadav11223@gmail.com>
2025-11-06 09:37:51 +0530

    Array example

---
+++ Array.js
@@ -0,0 +1,6 @@
+@REM let student=["Monika","moni","Satyam","Mohit","Nitya"];
+@REM let marks=[80,79,20,40,30];
+@REM const studentsBody=document.getElementById("studentsBody");
+@REM const output=document.getElementById("output");
+@REM const input=document.getElementById("input");
+
